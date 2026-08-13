deb98bb6c81ed5d82a1099876487c534ea23cd45
didi <didi@MacBook-Pro-113.local>
2026-08-13 20:26:01 +0800

    BR_train_din_semantic_residual_query: add gated semantic DIN queries

--- model.py
+++ model.py
@@ -109,6 +109,20 @@ class Model(tf.keras.Model):
         self.seq_pay_attention_layer = DIN_attention_Layer([50, 20], 'sigmoid', name='global_pay_seq')
         self.seq_12h_click_cate_id_attention_layer = DIN_attention_Layer([50, 20], 'sigmoid',
                                                                          name='12h_click_cate_id_seq')
+        # 保留 slot 68 的原始 8 维 query，仅用近零门控叠加语义增量，避免一次性替换造成信息损失。
+        seq_query_reg = regularizers.l2(model_conf.l2_reg)
+        self.shop_seq_query_delta = tf.keras.layers.Dense(
+            model_conf.fm_emb_size, activation=tf.nn.swish,
+            kernel_regularizer=seq_query_reg, name='shop_seq_query_delta')
+        self.category_seq_query_delta = tf.keras.layers.Dense(
+            model_conf.fm_emb_size, activation=tf.nn.swish,
+            kernel_regularizer=seq_query_reg, name='category_seq_query_delta')
+        self.shop_seq_query_gate = self.add_weight(
+            name='shop_seq_query_gate', shape=(),
+            initializer=tf.keras.initializers.Constant(0.01), trainable=True)
+        self.category_seq_query_gate = self.add_weight(
+            name='category_seq_query_gate', shape=(),
+            initializer=tf.keras.initializers.Constant(0.01), trainable=True)
         self.attention_layer_search_long_pay = DIN_attention_Layer([50, 20], 'sigmoid', name='search_pay_seq_long')
         self.attention_layer_search_long_clk = DIN_attention_Layer([50, 20], 'sigmoid', name='search_clk_seq_long')
         self.attention_layer_search_long_query = DIN_attention_Layer([50, 20], 'sigmoid', name='search_query_seq_long')
@@ -505,6 +519,26 @@ class Model(tf.keras.Model):
             global_query_input,
             [tf.shape(global_query_input)[0], len(model_conf.global_seq_query_sids) * model_conf.fm_emb_size])
 
+        shop_query_slot_indices = self.slot_id_table.lookup(
+            tf.constant(model_conf.shop_seq_query_delta_sids, dtype=tf.dtypes.int32))
+        shop_query_delta = tf.gather(pooled_output[:, :, 1:], shop_query_slot_indices, axis=1)
+        shop_query_delta = tf.reshape(
+            shop_query_delta,
+            [tf.shape(shop_query_delta)[0],
+             len(model_conf.shop_seq_query_delta_sids) * model_conf.fm_emb_size])
+        shop_query_input = global_query_input + self.shop_seq_query_gate * self.shop_seq_query_delta(shop_query_delta)
+
+        category_query_slot_indices = self.slot_id_table.lookup(
+            tf.constant(model_conf.category_seq_query_delta_sids, dtype=tf.dtypes.int32))
+        category_query_delta = tf.gather(pooled_output[:, :, 1:], category_query_slot_indices, axis=1)
+        category_query_delta = tf.reshape(
+            category_query_delta,
+            [tf.shape(category_query_delta)[0],
+             len(model_conf.category_seq_query_delta_sids) * model_conf.fm_emb_size])
+        category_query_input = (
+            global_query_input
+            + self.category_seq_query_gate * self.category_seq_query_delta(category_query_delta))
+
         # pooled_output_v2, slot_mask_v2 = self.process_and_pool_fused(sid_list, fid_list, table_type='din_ads_table')
         # ads_slot_indices = self.slot_id_table_din_ads.lookup(tf.constant(model_conf.ads_fea_slots, dtype=tf.dtypes.int32))
         # ads_emb = tf.gather(pooled_output_v2, ads_slot_indices, axis=1)
@@ -516,12 +550,12 @@ class Model(tf.keras.Model):
             seq_input = tf.gather(pooled_output[:, :, 1:], seq_slot_indices, axis=1)
             seq_mask = tf.gather(slot_mask, seq_slot_indices, axis=1)
             if seq_name == 'user_click_seq':
-                seq_output = self.seq_click_attention_layer([global_query_input, seq_input, seq_input, seq_mask])
+                seq_output = self.seq_click_attention_layer([shop_query_input, seq_input, seq_input, seq_mask])
             elif seq_name == 'user_pay_seq':
-                seq_output = self.seq_pay_attention_layer([global_query_input, seq_input, seq_input, seq_mask])
+                seq_output = self.seq_pay_attention_layer([shop_query_input, seq_input, seq_input, seq_mask])
             elif seq_name == 'user_12h_click_cateid':
                 seq_output = self.seq_12h_click_cate_id_attention_layer(
-                    [global_query_input, seq_input, seq_input, seq_mask])
+                    [category_query_input, seq_input, seq_input, seq_mask])
             seq_outputs.append(seq_output)
 
         # 搜索支付序列
@@ -600,4 +634,3 @@ class Model(tf.keras.Model):
             return final_pred, cvr_score, ctr_score, cat_score, ext_score
 
         return ctcvr, cat_pred, click_pred, ext_pred
-
