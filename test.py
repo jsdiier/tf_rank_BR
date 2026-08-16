4c7397dba8f70ca1e96964b174c09c523b4abee7
didi <didi@MacBook-Pro-113.local>
2026-08-16 19:03:44 +0800

    BR_train_buy_ctr_exponent_alpha: final BUY = CTR^1.5 x CVR

--- test.py
+++ test.py
@@ -159,9 +159,10 @@ class Learner:
         sum_pred_ext = 0.0
         for step, feat in enumerate(test_data):
             cnt += feat['cvr_label'].shape[0]
-            pred_buy, pred_cat, pred_click, pred_ext = self.model([feat['fea_ids'], feat['fea_vals']])
+            pred_buy_for_loss, pred_buy, pred_cat, pred_click, pred_ext = self.model(
+                [feat['fea_ids'], feat['fea_vals']])
 
-            loss_buy = self.model.loss_bc(tf.expand_dims(feat['cvr_label'], 1), pred_buy)
+            loss_buy = self.model.loss_bc(tf.expand_dims(feat['cvr_label'], 1), pred_buy_for_loss)
             loss_cat = self.model.loss_bc(tf.expand_dims(feat['cat_label'], 1), pred_cat)
             loss_click = self.model.loss_bc(tf.expand_dims(feat['clk_label'], 1), pred_click)
             loss_ext = self.model.loss_bc(tf.expand_dims(feat['ext_label'], 1), pred_ext)
