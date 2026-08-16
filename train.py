183b02debb4de86803cb7e76ca715caefebc5ada
didi <didi@MacBook-Pro-113.local>
2026-08-16 19:10:34 +0800

    BR_train_buy_combo_cat_fusion: CTRxCAT + weak-positive combo + funnel consistency

--- train.py
+++ train.py
@@ -12,6 +12,12 @@ from tensorflow.keras import regularizers
 import time
 from sklearn import metrics
 
+
+def _safe_logit(prob):
+    prob = tf.clip_by_value(prob, 1e-7, 1.0 - 1e-7)
+    return tf.math.log(prob / (1.0 - prob))
+
+
 class Learner:
     def __init__(self):
         self.model = None
@@ -24,14 +30,21 @@ class Learner:
     def train_step(self, feat, buy_weight=1.0, cat_weight=1.0, click_weight=1.0, ext_weight=1.0):
         model = self.model
         with tf.GradientTape() as tape:
-            pred_buy, pred_cat, pred_click, pred_ext = model([feat['fea_ids'], feat['fea_vals']])
+            pred_buy_for_loss, pred_buy, pred_cat, pred_click, pred_ext = model(
+                [feat['fea_ids'], feat['fea_vals']])
 
-            loss_buy = model.loss_bc(tf.expand_dims(feat['cvr_label'], 1), pred_buy)
+            loss_buy = model.loss_bc(tf.expand_dims(feat['cvr_label'], 1), pred_buy_for_loss)
             loss_cat = model.loss_bc(tf.expand_dims(feat['cat_label'], 1), pred_cat)
             loss_click = model.loss_bc(tf.expand_dims(feat['clk_label'], 1), pred_click)
             loss_ext = model.loss_bc(tf.expand_dims(feat['ext_label'], 1), pred_ext)
 
             final_loss = loss_buy * buy_weight + loss_cat * cat_weight + loss_click * click_weight + loss_ext * ext_weight
+            if model_conf.funnel_order_weight > 0.0:
+                funnel_reg = (
+                    tf.math.softplus(_safe_logit(pred_buy) - _safe_logit(pred_cat))
+                    + tf.math.softplus(_safe_logit(pred_cat) - _safe_logit(pred_click))
+                )
+                final_loss = final_loss + model_conf.funnel_order_weight * funnel_reg
 
             gradients = tape.gradient(final_loss, model.trainable_weights)
         model.optimizer.apply_gradients(zip(gradients, model.trainable_weights))
